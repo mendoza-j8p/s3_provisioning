b62db6d57247f4d873d86ccb2b38d8d873342799
Jenifer Mendoza <mendoza.j8p@gmail.com>
2023-05-06 10:34:30 +0200

    add delete_full

---
+++ delete_full_bucket.py
@@ -0,0 +1,25 @@
+import boto3
+from aws_session import get_session
+
+session = get_session()
+
+# Creación de un cliente de servicio
+s3 = session.client('s3')
+
+# Obtener la lista de buckets
+response = s3.list_buckets()
+buckets = [bucket['Name'] for bucket in response['Buckets']]
+
+# Eliminación de los buckets con contenido
+for bucket in buckets:
+    try:
+        objects = s3.list_objects(Bucket=bucket)
+        if 'Contents' in objects:
+            delete_keys = {'Objects': []}
+            for obj in objects['Contents']:
+                delete_keys['Objects'].append({'Key': obj['Key']})
+            s3.delete_objects(Bucket=bucket, Delete=delete_keys)
+        s3.delete_bucket(Bucket=bucket)
+        print(f"Bucket {bucket} eliminado correctamente")
+    except s3.exceptions.BucketNotEmpty:
+        print(f"El bucket {bucket} tiene contenido, no se puede eliminar")
